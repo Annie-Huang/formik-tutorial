b8d79325ba24a563850f69b556426f7d36991cd8
Annie Huang <annie.hn@gmail.com>
2025-06-15 20:38:34 +1000

    Use formik in BasicForm.tsx

--- src/components/BasicForm.tsx
+++ src/components/BasicForm.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { useFormik } from 'formik';
 
 const BasicForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  console.log(formik);
+
   return (
     <form autoComplete='off'>
       <label htmlFor='email'>Email</label>
-      <input id='email' type='email' placeholder='Enter your email' />
+      {/*<input id='email' type='email' placeholder='Enter your email' />*/}
+      <input
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        id='email'
+        type='email'
+        placeholder='Enter your email'
+      />
     </form>
   );
 };

1b85f1d47be147d528a6858474197ce95960d2a5
Annie Huang <annie.hn@gmail.com>
2025-06-20 22:06:48 +1000

    Use <Form> instead of <form>, use <Field> instead of <input>

--- src/components/AdvancedForm.tsx
+++ src/components/AdvancedForm.tsx
@@ -1,19 +1,20 @@
-import { Formik } from 'formik';
+import { Field, Form, Formik } from 'formik';
 
 const AdvancedForm = () => {
   return (
     <Formik initialValues={{ name: 'jared' }}>
       {(props) => (
-        <form onSubmit={props.handleSubmit}>
-          <input
-            type='text'
-            onChange={props.handleChange}
-            onBlur={props.handleBlur}
-            value={props.values.name}
-            name='name'
-          />
+        <Form>
+          {/*<input*/}
+          {/*  type='text'*/}
+          {/*  onChange={props.handleChange}*/}
+          {/*  onBlur={props.handleBlur}*/}
+          {/*  value={props.values.name}*/}
+          {/*  name='name'*/}
+          {/*/>*/}
+          <Field type='text' name='name' placeholder='Name' />
           <button type='submit'>Submit</button>
-        </form>
+        </Form>
       )}
     </Formik>
   );

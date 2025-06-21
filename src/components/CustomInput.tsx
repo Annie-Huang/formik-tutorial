27d1c23b6f3306c759a4fc3621ced46ce37b1c2d
Annie Huang <annie.hn@gmail.com>
2025-06-21 16:36:43 +1000

    Add the error style and text with the CustomInput component

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -15,7 +15,12 @@ const CustomInput: FC<CustomInputProps> = ({ label, ...props }) => {
   return (
     <>
       <label>{label}</label>
-      <input {...field} {...props} />
+      <input
+        {...field}
+        {...props}
+        className={meta.touched && meta.error ? 'input-error' : ''}
+      />
+      {meta.touched && meta.error && <div className='error'>{meta.error}</div>}
     </>
   );
 };
